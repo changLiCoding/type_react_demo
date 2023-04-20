2e33fdfbe42f30c8cb444cd041cd59ff1000696f
javascriptsucks <evix1988@live.cn>
2023-04-19 19:18:43 -0600

    download schema from anilist

--- src/App.tsx
+++ src/App.tsx
@@ -1,91 +1,39 @@
-import React, { useCallback, useEffect, useState, useRef } from "react";
+import React from "react";
+import { Character, Characters, Query } from "./__generated__/graphql";
+import { useQuery } from "@apollo/client";
 
-import List from "./components/List";
-import Heading from "./components/Heading";
-import Incrementer from "./components/Incrementer";
-import Box from "./components/Box";
+import { gql } from "./__generated__/gql";
 
-import { useSelector, useDispatch } from "react-redux";
-import { selectTodos } from "./app/store";
-import { addTodo, removeTodo } from "./app/features/todoSlice";
-import {
-	useTodosManager,
-	useAddTodo,
-	useRemoveTodo,
-	useTodos,
-} from "./hooks/useTodos";
-import { useNumber } from "./hooks/useNumber";
-import "./App.css";
-import { createGlobalState } from "react-use";
-import { Todo, TodosSliceState } from "./app/features/todoSlice";
-
-export type PayLoad = {
-	text: string;
-};
-
-export const items = ["one", "two", "three"];
-
+const allCharacters = gql(`
+	query {
+		characters {
+			results {
+				name
+				status
+				species
+				gender
+				image
+				location {
+					name
+				}
+			}
+		}
+	}
+`);
 function App() {
-	const todos = useSelector(selectTodos);
-	const dispatch = useDispatch();
-	// const todos = useTodos();
-	// const addTodo = useAddTodo();
-	// const removeTodo = useRemoveTodo();
-
-	const [payload, setPayload] = useState<PayLoad | null>({
-		text: "Inital value",
-	});
-
-	const newTodoRef = useRef<HTMLInputElement>(null);
-	// const addTodo = useCallback(() => {
-	// 	dispatch({ type: "ADD", todo: newTodoRef.current?.value ?? "" });
-	// }, []);
-
-	useEffect(() => {
-		fetch("/data.json")
-			.then((response) => response.json())
-			.then((data) => setPayload(data));
-	}, []);
-
-	// const onListItemClick = useCallback((item: string): void => {
-	// 	alert(item);
-	// }, []);
-	const onListItemClick = (item: string): void => {
-		dispatch(addTodo(item));
-		alert(item);
-	};
-
-	const [value, setValue] = useNumber(0);
+	const { loading, error, data } = useQuery(allCharacters);
 	return (
 		<div className='App'>
-			<Heading title='Introduction' />
-			<Box>Hello there</Box>
-			<List
-				items={items}
-				onClick={onListItemClick}
-			/>
-			<Box>{payload?.text}</Box>
-			{todos?.map((todo, index) => (
-				<Box key={index}>
-					{todo.todo}
-					<button onClick={() => dispatch(removeTodo(todo.id))}>Remove</button>
-				</Box>
-			))}
-			<div>
-				<input
-					type='text'
-					ref={newTodoRef}
-				/>
-				<button
-					onClick={() => dispatch(addTodo(newTodoRef.current?.value ?? ""))}>
-					Add Todo
-				</button>
-			</div>
-			<Incrementer
-				value={value}
-				setValue={setValue}
-			/>
+			{loading && <p>Loading...</p>}
+			{error && <p>Error: {error.message}</p>}
+
+			<ul>
+				{data?.characters.map((character: Character) => (
+					<li key={character.name}>{character.name}</li>
+				))}
+			</ul>
 		</div>
 	);
 }
+
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -4,21 +4,36 @@ import App from "./App";
 import store from "./app/store";
 import { Provider } from "react-redux";
 import { TodoProvider } from "./hooks/useTodos";
+
+import {
+	ApolloClient,
+	InMemoryCache,
+	ApolloProvider,
+	gql,
+} from "@apollo/client";
+
 import "./index.css";
+
+const client = new ApolloClient({
+	uri: "https://anilist.co/graphql",
+	cache: new InMemoryCache(),
+});
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 	<React.StrictMode>
-		<TodoProvider
-			initialTodos={[
-				{
-					id: 999,
-					todo: "Get Up Early",
-					done: false,
-				},
-			]}>
-			<Provider store={store}>
-				<App />
-			</Provider>
-		</TodoProvider>
+		<ApolloProvider client={client}>
+			<TodoProvider
+				initialTodos={[
+					{
+						id: 999,
+						todo: "Get Up Early",
+						done: false,
+					},
+				]}>
+				<Provider store={store}>
+					<App />
+				</Provider>
+			</TodoProvider>
+		</ApolloProvider>
 	</React.StrictMode>
 );
