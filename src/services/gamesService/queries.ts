d0d69c31289338bc8308881168039ca2f9408bfa
javascriptsucks <evix1988@live.cn>
2023-04-21 10:52:06 -0600

    ready for vitest

--- src/services/gamesService/queries.ts
+++ src/services/gamesService/queries.ts
@@ -9,3 +9,19 @@ export const ALL_GAMES = gql`
 		}
 	}
 `;
+
+export const GAMES_FOR_A_USER = gql`
+	query {
+		gamesForAUser {
+			name
+			imageURL
+			genres
+			platforms
+		}
+	}
+`;
+
+export const ADD_USER_GAMES = gql`
+  mutation AddUserGames($user_id: Int!, $game_id: Int!) {
+    addUserGames(input: $) {
+  }`;
